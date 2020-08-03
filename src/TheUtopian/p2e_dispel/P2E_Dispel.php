c5becd236b3d652f6bb119310d02edee4efa6cfd
matthewmenser@gamil.com <a19982809>
2020-08-03 12:12:48 -0500

    add p2e_dispel\command and its contents

--- src/TheUtopian/p2e_dispel/P2E_Dispel.php
+++ src/TheUtopian/p2e_dispel/P2E_Dispel.php
@@ -8,43 +8,21 @@ use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 use pocketmine\Player;
 use pocketmine\plugin\PluginBase;
+use TheUtopian\p2e_dispel\command\DispelCommand;
 
 class P2E_Dispel extends PluginBase
 {
-    public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool
-    {
-        if (!$sender instanceof Player) {
-            $sender->sendMessage("§cThis command can be used only in game!");
-            return true;
-        }
-        if (!isset($args[0])) {
-            $sender->sendMessage("§o§l§c/dispel (enchantment)");
-            return true;
-        }
-        switch ($args[0]) {
-            case "dispel":
-                $enchant = CustomEnchantManager::getEnchantmentByName($args[0]);
-                $item = $sender->getInventory()->getItemInHand();
-                $nope = [118, 108];
-                if ($enchant === null) {
-                    $sender->sendMessage("§o§l§cInvalid enchantment!");
-                    return true;
-                }
-                if ($item->getEnchantment($enchant->getId()) === null) {
-                    $sender->sendMessage("§o§l§cItem does not have specified enchantment!");
-                    return true;
-                }
-                if ($item->getEnchantment($enchant->getId()) === $nope) {
-                    $sender->sendMessage("§o§l§cThat Enchant cannot be Dispelled!");
-                    return true;
-                }
-                $item->removeEnchantment($enchant->getId());
-                if (count($item->getEnchantments()) === 0) $item->removeEnchantments();
-                $sender->sendMessage("§o§l§aEnchantment successfully removed.");
-                $sender->getInventory()->setItemInHand($item);
-                return true;
-        }
+    private static $instance;
 
-        return true;
+    public function onEnable(){
+        self::$instance = $this;
+        $this->getServer()->getCommandMap()->register("p2e_dispel", new DispelCommand());
+    }
+
+    /**
+     * @return mixed
+     */
+    public static function getInstance(){
+        return self::$instance;
     }
 }
---
+++ src/TheUtopian/p2e_dispel/command/DispelCommand.php
@@ -0,0 +1,75 @@
+<?php
+declare(strict_types=1);
+
+namespace TheUtopian\p2e_dispel\command;
+
+
+use DaPigGuy\PiggyCustomEnchants\CustomEnchantManager;
+use DaPigGuy\PiggyCustomEnchants\enchants\CustomEnchantIds;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\command\PluginIdentifiableCommand;
+use pocketmine\item\Item;
+use pocketmine\item\ItemFactory;
+use pocketmine\Player;
+use pocketmine\plugin\Plugin;
+use TheUtopian\p2e_dispel\P2E_Dispel;
+
+class DispelCommand extends Command implements PluginIdentifiableCommand{
+
+    private $notAllowed = [CustomEnchantIds::AUTOREPAIR, CustomEnchantIds::SOULBOUND];
+
+    public function __construct(){
+        parent::__construct("dispel", "Remove spells from items", "/dispel <enchantName>");
+    }
+
+    public function execute(CommandSender $sender, string $commandLabel, array $args){
+        if (!$sender instanceof Player) {
+            $sender->sendMessage("§cThis command can be used only in game!");
+            return true;
+        }
+        if (!isset($args[0])) {
+            $sender->sendMessage("§o§l§c/dispel (enchantment)");
+            return true;
+        }
+
+        $testEnchant = CustomEnchantManager::getEnchantmentByName($args[0]);
+
+        if ($testEnchant === null) {
+            $sender->sendMessage("§o§l§cInvalid enchantment " . $args[0] . "!");
+            return true;
+        }
+
+        if (in_array($testEnchant->getId(), $this->notAllowed)) {
+            $sender->sendMessage("§o§l§cThat Enchant cannot be Dispelled!");
+            return true;
+        }
+
+        $item = $sender->getInventory()->getItemInHand();
+        $currentEnchant = $item->getEnchantment($testEnchant->getId());
+
+        if ($currentEnchant === null) {
+            $sender->sendMessage("§o§l§cItem does not have specified enchantment!");
+            return true;
+        }
+
+        $book = ItemFactory::get(Item::ENCHANTED_BOOK);
+        $book->addEnchantment($currentEnchant);
+        $inventory = $sender->getInventory();
+        if(!$inventory->canAddItem($book)){
+            $sender->sendMessage("Not enough room in your inventory!");
+            return;
+        }
+
+        $item->removeEnchantment($testEnchant->getId());
+        $inventory->setItemInHand($item);
+        $inventory->addItem($book);
+        $sender->sendMessage("§o§l§aEnchantment successfully removed.");
+        return true;
+    }
+
+    public function getPlugin() : Plugin{
+        return P2E_Dispel::getInstance();
+    }
+
+}
